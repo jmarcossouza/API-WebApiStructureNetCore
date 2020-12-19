8bdabca8dc59a15f45a1398c72b59a739492c6a3
João Marcos A. Souza <contato@jmarcossouza.com>
2020-12-19 13:32:40 -0300

    entity framework e migration initial funcionando

---
+++ WebApiStructureNetCore/Data/WebapiStructureDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiStructureNetCore.Models;
+
+namespace WebApiStructureNetCore.Data
+{
+    public class WebapiStructureDbContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=WebapiStructureDatabase;Data Source=DESKTOP-5DKEVPB");
+        }
+    }
+}
---
+++ WebApiStructureNetCore/Migrations/20201219162049_Initial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApiStructureNetCore.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "varchar(128)", nullable: false),
+                    Email = table.Column<string>(type: "varchar(254)", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(35)", nullable: false),
+                    Sobrenome = table.Column<string>(type: "nvarchar(35)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
