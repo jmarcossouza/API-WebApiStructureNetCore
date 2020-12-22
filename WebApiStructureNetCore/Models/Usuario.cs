816cbb69f71badbc08ea9a6587e1665f0d6d05d6
João Marcos A. Souza <contato@jmarcossouza.com>
2020-12-21 22:15:15 -0300

    CRUD do usuario funcionando perfeitamente

--- WebApiStructureNetCore/Models/Usuario.cs
+++ WebApiStructureNetCore/Models/Usuario.cs
@@ -7,45 +7,49 @@ using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiStructureNetCore.Models
 {
     public class Usuario : PasswordHasher<Usuario>
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public string Id { get; set; }
-
+        /// <summary>Quando criando um novo usuário, sempre enviar com valor 0</summary>
+        public long Id { get; set; }
+        [Required]
+        [MaxLength(254)]
         public string Email { get; set; }
-
+        [Column("Senha")]
+        private string _senha;
         [Required]
         [MinLength(8)]
-        public string Senha { get
-            {
-                return Senha;
-            }
+        [MaxLength(84)]
+        public string Senha { 
+            get => _senha;
             set
             {
-                Senha = HashPassword(this, value);
+                _senha = HashPassword(this, value);
             }
         }
-
+        [Required]
+        [MinLength(2)]
+        [MaxLength(35)]
         public string Nome { get; set; }
-
+        [Required]
+        [MinLength(2)]
+        [MaxLength(35)]
         public string Sobrenome { get; set; }
-        public DateTime CriadoEm { get
-            {
-                return CriadoEm;
-            }
-            private set
-            {
-                CriadoEm = DateTime.Now;
-            }
+        public DateTime CriadoEm { get; private set; }
+
+        public Usuario()
+        {
+            CriadoEm = DateTime.Now;
         }
 
         public bool verifyPassword(string passToBeVerified)
         {
             return (VerifyHashedPassword(this, Senha, passToBeVerified) == PasswordVerificationResult.Success);
         }
-
     }
 }
